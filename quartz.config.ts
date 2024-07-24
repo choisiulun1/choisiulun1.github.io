2ab9e13ffa700ffccc186e8a6662f40ae5636b56
Siulun <36674999+choisiulun1@users.noreply.github.com>
2024-07-24 22:15:14 +0800

    Quartz sync: Jul 24, 2024, 10:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "md.siulun1.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
